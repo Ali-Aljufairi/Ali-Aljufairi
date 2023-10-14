986bbf8480178d24165ebc4d7e37113d5f071fca
Ali-Aljufairi <ali.redha.ali@gmail.com>
2023-10-14 17:18:08 +0300

    Fixed input python

--- .github/workflows/process_gif.py
+++ .github/workflows/process_gif.py
@@ -43,7 +43,7 @@ class GifBackgroundRemover:
         )
 
 if __name__ == "__main__":
-    input_path = "ocean.gif"
+    input_path = "output/ocean.gif"
     output_path = "output/ocean.gif"
     skip_frames = 5  # Number of frames to skip at the beginning
 
